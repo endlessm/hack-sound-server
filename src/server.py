46abb8d033e67690ad373dc519442a5f51ebcb53
Fabian Orccon <cfoch.fabian@gmail.com>
2019-01-17 10:40:28 -0500

    server: Add get_player method
    https://phabricator.endlessm.com/T24927

--- src/server.py
+++ src/server.py
@@ -399,6 +399,13 @@ class HackSoundServer(Gio.Application):
             self._background_players.append(player)
         player.play()
 
+    def get_player(self, uuid_):
+        try:
+            return self.players[uuid_]
+        except KeyError:
+            self.logger.critical("This uuid is not assigned to any player.",
+                                 uuid=uuid_)
+
     def refcount(self, uuid_, bus_name=None):
         if bus_name is None:
             refcount = 0
@@ -430,7 +437,7 @@ class HackSoundServer(Gio.Application):
             # Only stop the sound if the last bus name (application) referring
             # to it has been disconnected (closed). The stop method will,
             # indirectly, take care for deleting self._refcount[uuid_].
-            self.players[uuid_].stop()
+            self.get_player(uuid_).stop()
 
     def do_dbus_register(self, connection, path):
         Gio.Application.do_dbus_register(self, connection, path)
@@ -544,7 +551,7 @@ class HackSoundServer(Gio.Application):
 
         if overlap_behavior == "restart":
             # This behavior indicates to restart the sound.
-            self.players[uuid_].reset()
+            self.get_player(uuid_).reset()
             return uuid_
         elif overlap_behavior == "ignore":
             # If a sound is already playing, then ignore the new one.
@@ -598,7 +605,8 @@ class HackSoundServer(Gio.Application):
             self.logger.info("Properties of sound {} was supposed to be "
                              "updated, but did not exist".format(uuid_))
         else:
-            self.players[uuid_].update_properties(transition_time_ms, options)
+            player = self.get_player(uuid_)
+            player.update_properties(transition_time_ms, options)
         invocation.return_value(None)
 
     def __method_called_cb(self, connection, sender, path, iface,
