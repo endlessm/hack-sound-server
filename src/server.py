fe430497c1723d3c93117281711d6c1b5842708d
Fabian Orccon <cfoch.fabian@gmail.com>
2019-04-11 12:33:50 -0500

    server: Remove check for valid overlap behavior
    CI ensures now that sounds in the metadata JSON file have the
    right overlap behavior tag.
    
    https://phabricator.endlessm.com/T25991

--- src/server.py
+++ src/server.py
@@ -160,13 +160,6 @@ class Server(Gio.Application):
 
         overlap_behavior = \
             self.metadata[sound_event_id].get("overlap-behavior", "overlap")
-        if overlap_behavior not in self.OVERLAP_BEHAVIOR_CHOICES:
-            msg = "'%s' is not a valid option for 'overlap-behavior'."
-            self.logger.info(msg, overlap_behavior,
-                             sound_event_id=sound_event_id)
-            return invocation.return_dbus_error(
-                self._DBUS_UNKNOWN_OVERLAP_BEHAVIOR,
-                msg % overlap_behavior)
 
         uuid_ = self.do_overlap_behaviour(sound_event_id, overlap_behavior)
         if uuid_ is not None:
