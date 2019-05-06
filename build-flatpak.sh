a0ba4d7f638318ad4b8eae8d884d7314b8aa9a20
Fabian Orccon <cfoch.fabian@gmail.com>
2019-05-06 15:26:23 -0500

    tools: flatpak: Add script to build a local flatpak
    Copy&pasted from clubhouse/tools/build-flatpak.sh
    
    https://phabricator.endlessm.com/T26556

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e
 set -x
+
 rm -rf files var metadata export build
 
-BRANCH=${BRANCH:-master}
-GIT_CLONE_BRANCH=${GIT_CLONE_BRANCH:-HEAD}
-
-sed \
-  -e "s|@BRANCH@|${BRANCH}|g" \
-  -e "s|@GIT_CLONE_BRANCH@|${GIT_CLONE_BRANCH}|g" \
-  com.endlessm.HackSoundServer.json.in \
-  > com.endlessm.HackSoundServer.json
-
-flatpak-builder build --force-clean --install com.endlessm.HackSoundServer.json
-
+source_dir="$(git rev-parse --show-toplevel)"
+GIT_CLONE_BRANCH="HEAD" ./tools/build-local-flatpak.sh --install
